3b89ec16774efe2fc7f19839c77c2ffd8d8d023d
Isco8809 <francisco.echeverri@bambooanalytics.com.co>
2021-11-02 20:42:08 -0500

    Leccion 4

--- Javascript/Ejercicio2/Leccion4.js
+++ Javascript/Ejercicio2/Leccion4.js
@@ -50,3 +50,42 @@ contador
 
 //Ejercicio 4
 
+var donaciones = [5,15,22,25,30,52,5,1,0];
+
+function minimmo (arr) {
+    var k = 0;
+    for (var i = 0 ; i < arr.length ; i++){
+        if (arr[k] >= arr[i+1]){
+            k = i+1;
+            arr[k] = arr[i+1];
+        }
+    }
+    return arr[k];
+}
+
+function maximo (arr) {
+    var k = 0;
+    for (var i = 0 ; i < arr.length ; i++){
+        if (arr[k] <= arr[i+1]){
+            k = i+1;
+            arr[k] = arr[i+1];
+        }
+    }
+    return arr[k];
+}
+
+
+function promedio (min , max , donaciones){
+    var suma = 0;
+    var cantidad = 0;
+    for (var elemento of donaciones){
+        if(elemento != min && elemento != max){
+            suma = suma + elemento;
+            cantidad ++;
+        }
+    }
+    return suma/cantidad;
+}
+var min = minimmo(donaciones);
+var max = maximo(donaciones);
+console.log(promedio(min,max,donaciones))
